52a15debb02c8812e395af2ae5105bbc9ed2331f
Stratos Skarlatos <haxemaniac1999@gmail.com>
2022-12-18 02:15:44 +0200

    lot of shit
    express js for sessions
    schema sql function fix

--- schema.sql
+++ schema.sql
@@ -80,15 +80,15 @@ RETURNS void
 LANGUAGE plpgsql
 AS $function$
 BEGIN
-    UPDATE board SET move1 = hand WHERE id = boardid AND player = 1;
-    UPDATE board SET move2 = hand WHERE id = boardid AND player = 2;
+    UPDATE board SET move1 = hand::move WHERE id = boardid AND player = 1;
+    UPDATE board SET move2 = hand::move WHERE id = boardid AND player = 2;
 	PERFORM score_players(boardid);
 END;
 $function$;
 
 ----------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION score_players(board_id int)
+CREATE OR REPLACE FUNCTION score_players(bid int)
 RETURNS Boolean
 LANGUAGE plpgsql
 AS $function$
@@ -98,32 +98,43 @@ AS $function$
     s1 INT;--score of player 1 
 	s2 INT;--score of player 2
 BEGIN
-    p1 := (select b.move1 from board b where b.id=board_id);
-	p2 := (select b.move2 from board b where b.id=board_id);
+    p1 := (select b.move1 from board b where b.id=bid);
+	p2 := (select b.move2 from board b where b.id=bid);
 	
-    IF p1='EMPTY' || p2='EMPTY' THEN
+    IF p1='EMPTY' or p2='EMPTY' THEN
     RETURN FALSE;
     END IF;
 	
-	s1 :=(select g.pl1_score from game_status g where board_id=g.board_id);
-	s2 :=(select g.pl2_score from game_status g where board_id=g.board_id);
+	s1 :=(select pl1_score from game_status where bid=game_status.board_id);
+	s2 :=(select pl2_score from game_status where bid=game_status.board_id);
 	
 	IF s1=3 or s2=3 then return false;END IF;
 	
     IF p1='r' AND p2 ='s'
-	THEN s1=s1+1;
+	THEN 
+	s1=s1+1;
+	update game_status set pl1_score=s1 where board_id=bid;
 	ELSIF p1='s' AND p2 ='p'
-	THEN s1=s1+1;
+	THEN 
+	s1=s1+1;
+	update game_status set pl1_score=s1 where board_id=bid;
 	ELSIF p1='p' AND p2 ='r'
 	THEN s1=s1+1;
+	update game_status set pl1_score=s1 where board_id=bid;
 	ELSIF p2='r' AND p1 ='s'
 	THEN s2=s2+1;
+	update game_status set pl2_score=s2 where board_id=bid;
 	ELSIF p2='s' AND p1 ='p'
-	THEN s2=s2+1;
+	THEN 
+	s2=s2+1;
+	update game_status set pl2_score=s2 where board_id=bid;
 	ELSIF p2='p' AND p1 ='r'
-	THEN s2=s2+1;
+	THEN 
+	s2=s2+1;
+	update game_status set pl2_score=s2 where board_id=bid;
 	END IF;
-	
+	update board set move1='EMPTY',move2='EMPTY';
+	RETURN TRUE;
 END;
 $function$;
 
@@ -134,7 +145,10 @@ insert into users(username) values('stratos');
 select * from users;
 insert into board(player1id, player2id) values(1,2);
 select * from board;
+insert into game_status(status,board_id,pl1_score,pl2_score,last_change) values ('initialized',1,0,0,now());
+select * from game_status;
 SELECT play_hand('r',1,1);
 SELECT play_hand('p',2,1);
+select score_players(1);
 
 --test--test--test--test--test--test--test--test--test
