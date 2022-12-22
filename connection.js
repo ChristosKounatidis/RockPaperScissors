1cab41c6bf40600e9721918fa51213c7af9884f2
Christos Kounatidis <41286370+ChristosKounatidis@users.noreply.github.com>
2022-12-22 20:55:58 +0000

    replit test not working yet

--- connection.js
+++ connection.js
@@ -1,4 +1,16 @@
-credentials = require('./credentials.js');
+const user = process.env['user'];
+const host = process.env['host'];
+const database = process.env['database'];
+const password = process.env['password'];
+const port = process.env['port'];
+
+console.log(user);
+console.log(host);
+console.log(database);
+console.log(password);
+console.log(port);
+
+//credentials = require('./credentials.js');
 const { Client } = require('pg');
 
 const client = new Client({
@@ -8,8 +20,11 @@ const client = new Client({
     password,
     port
 });
+const mySecret = process.env['user']
 
 client.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+      console.log("Connected!");
+    if (err) console.log("Not Connected");
+    
 });
